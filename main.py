4eacbf1725a1442fb2ea764892cf68c1d569cf62
Alexey <Fyodorov.aleksej@gmail.com>
2017-11-01 11:36:59 +0300

    USB eject update

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import sys
 
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtWidgets import QAction, QTableWidgetItem, QTableView, QHeaderView, QItemDelegate
+from PyQt5.QtWidgets import QAction, QTableWidgetItem, QTableView, QHeaderView, QItemDelegate, QAbstractItemView, \
+    QMessageBox
 
 from mainwindow import Ui_MainWindow
 
@@ -29,9 +30,12 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui.tableWidget.setHorizontalHeaderItem(1,header2)
         self.ui.tableWidget.setHorizontalHeaderItem(2,header3)
 
+        self.ui.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
         headers = self.ui.tableWidget.horizontalHeader()
         headers.setStretchLastSection(True)
+        self.appendText("usb","/media","123/3567/890")
 
+        self.ui.ejectButton.clicked.connect(self.eject)
 
     #action of refreshing all window
     def refresh(self):
@@ -44,41 +48,82 @@ class MyWin(QtWidgets.QMainWindow):
 
     def appendText(self, name, directory, size):
         item1 = QTableWidgetItem(name)
-        item1.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsEnabled|(not QtCore.Qt.ItemIsEditable))
+        item1.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
         item2 = QTableWidgetItem(directory)
-        item2.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsEnabled|(not QtCore.Qt.ItemIsEditable))
+        item2.setFlags(QtCore.Qt.ItemIsEnabled | Qt.ItemIsSelectable)
         item3 = QTableWidgetItem(size)
-        item3.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsEnabled|(not QtCore.Qt.ItemIsEditable))
+        item3.setFlags(QtCore.Qt.ItemIsEnabled | Qt.ItemIsSelectable)
         table = self.ui.tableWidget
+
         item = table.item(table.rowCount()-1,0)
         if (item != None):
+
             table.insertRow(table.rowCount())
+        if (table.rowCount() == 0):
+            table.setRowCount(1)
         table.setItem(table.rowCount() - 1, 0, item1)
         table.setItem(table.rowCount() - 1, 1, item2)
         table.setItem(table.rowCount() - 1, 2, item3)
 
     def refreshDirectories(self):
-        subprocess.call("ls /media/alexey > " + os.getcwd() + "/log.txt", shell=True)
-        logfile = open(os.getcwd() + "./log.txt", "r+")
-        current = [i for i in logfile]
-        for i in range(0, len(current)):
-            if current[i] != self.dirList[i]:
-                self.dirList = current
-                return current[i]
-        return None
+        #self.clearTable()
+        subprocess.call("lsblk -p | grep 'sdb[0-9]' > " + os.getcwd() + "/log.txt", shell=True)
+        logfile = open(os.getcwd() + "/log.txt", "r+")
+        usbList = []
+        for i in logfile:
+            s = re.split(r" +",i)
+            dev = s[0][s[0].index("/"):]
+            flag = -1
+            usbList.append(dev)
+            for index in range(self.ui.tableWidget.rowCount()):
+                if (self.ui.tableWidget.item(index, 0).text() == dev):
+                    flag = index
+            if (flag == -1):
+                mount = s[6]
+                subprocess.call("df -hm | grep " + dev + " > " + os.getcwd() + "/memory.txt", shell=True)
+                memoryfile = open(os.getcwd() + "/memory.txt")
+
+            # saving memory
+                memdict = {"all": 0, "free": 0, "used": 0}
+                line = memoryfile.readline()
+                result = re.split(r" +", line)
+                if (len(result) >= 3):
+                    memdict["all"] = memdict["all"] + int(result[1])
+                    memdict["used"] = memdict["used"] + int(result[2])
+                    memdict["free"] = memdict["free"] + int(result[3])
+                    self.appendText(dev, mount, str(memdict["all"]) + "/" + str(memdict["used"]) + "/" + str(memdict["free"]))
+                memoryfile.close()
+        for i in range(0, self.ui.tableWidget.rowCount()):
+            if (self.ui.tableWidget.item(i,0) != None):
+                if (not (self.ui.tableWidget.item(i,0).text() in usbList)):
+                    self.ui.tableWidget.removeRow(i)
+        logfile.close()
+
+    def clearTable(self):
+        self.ui.tableWidget.clearContents()
+        self.ui.tableWidget.setRowCount(1)
+
+    def eject(self):
+        indexes = self.ui.tableWidget.selectionModel().selectedRows()
+        for index in sorted(indexes):
+            subprocess.call("eject " + self.ui.tableWidget.item(index.row(),0).text() + " > " + os.getcwd() + "/eject.txt", shell=True)
+            ejectFile = open(os.getcwd() + "/eject.txt", "r+")
+            message = ejectFile.read()
+            if (message != None):
+                error = QMessageBox(self)
+                error.setText(message)
+
 
 if __name__=="__main__":
     app = QtWidgets.QApplication(sys.argv)
-    #window = MyWin()
+    window = MyWin()
 
-    #window.show()
+    window.show()
 
     #creating timer for refresh main window
-    #timer = QTimer()
-    #timer.timeout.connect(window.refresh)
-    #timer.start(100)
-
-
-
+    timer = QTimer()
+    timer.timeout.connect(window.refreshDirectories)
+    timer.start(1000)
 
     sys.exit(app.exec_())
+    exit()
